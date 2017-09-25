4bedf19c86f3d7981b1f3c3cbbb5d7e8ea395ea8
Shweta R Kumthekar <srk77@afsconnect1.njit.edu>
2017-09-25 19:49:38 -0400

    initial page

--- index.php
+++ index.php
@@ -1,51 +1,39 @@
 <?php
 
   $obj = new main();
-    $text = "my text";
-      $obj->printthis($text);
+  $text = "my text";
+  $obj->printthis($text);
         
-	  $array = array(1,2,3,4,5,6,7);
-	    $obj->printArray($array);
+  $array = array(1,2,3,4,5,6,7);
+  $obj->printArray($array);
 
-	      class main {
+ class main 
+ {
 
-	          public function __construct() {
+ public function __construct() 
+ {
 
-		        echo 'hello i\'m an Object </br>';
+ echo 'hello i\'m an Object </br>';
 
-			    }
+ }
 
-			        public function printthis($text) {
-				      echo '<h1>print function demo </h1>';
-				            print($text);
-					          echo '<hr>';
-						      }
-
-						          public function
-							  printArray($array) {
-							        echo '<h1>array
-								print
-								function</h1>';
-								      print_r($array);
-								            echo
-									    '<hr>';
-									        }
-										    
-										        public
-											function
-											__destruct()
-											{
-
-											      echo
-											      '</br>
-											      I\'m
-											      Done';
-
-											          }
-
-
-												    }
-
-
-
-												    ?>
+  public function printthis($text)  
+  {
+   echo '<h1>print function demo </h1>';
+   print($text);
+   echo '<hr>';
+  }
+  public function
+  printArray($array) 
+  {
+  echo '<h1>array print function</h1>';
+  print_r($array);
+  echo '<hr>';
+  }
+  public
+  function__destruct()
+  {
+    echo '</br> I\'m Done';
+  }
+  }
+?>
